cab34d22f12458d8ba771fbab7ac9eb16eb37053
Prof. Flavius Gorgônio <108832782+FlaviusGorgonio@users.noreply.github.com>
2022-12-06 01:05:09 -0300

    Cria listas de avaliação sem conflito de interesses

--- distribution.py
+++ distribution.py
@@ -46,6 +46,53 @@ for student in student_list:
     student_pos += 1
 
 print('Foram importados %d alunos'%len(students))
-for student in students:
-    print(student, students[student])
+# for student in students:
+#     print(student, students[student])
     
+print()
+# print(students.keys())
+list1 = list(students.keys())
+# print(type(list1))
+
+list2 = list1[:]
+shuffle(list2)
+diff = conflicting_lists(list1, list2)
+while diff:
+    shuffle(list2)
+    diff = conflicting_lists(list1, list2)
+# print(type(list2))
+
+list3 = list1[:]
+shuffle(list3)
+diff = conflicting_lists(list1, list3) or conflicting_lists(list2, list3)
+while diff:
+    shuffle(list3)
+    diff = conflicting_lists(list1, list3) or conflicting_lists(list2, list3)
+# print(type(list3))
+
+list4 = list1[:]
+shuffle(list4)
+diff = conflicting_lists(list1, list4) or conflicting_lists(list2, list4) or conflicting_lists(list3, list4)
+while diff:
+    shuffle(list4)
+    diff = conflicting_lists(list1, list4) or conflicting_lists(list2, list4) or conflicting_lists(list3, list4)
+# print(type(list4))
+
+# dist_list = [list1, list2, list3, list4]
+print()
+print("Distribuição da Avaliação")
+print()
+print(len(list1))
+print("Lista 1:", list1)
+print()
+print(len(list2))
+print("Lista 2:", list2)
+print()
+print(len(list3))
+print("Lista 3:", list3)
+print()
+print(len(list4))
+print("Lista 4:", list4)
+
+for i in range(len(list1)):
+    print("%s\n\t%s\n\t%s\n\t%s\n\n"%(students[list1[i]], students[list2[i]], students[list3[i]], students[list4[i]]))
